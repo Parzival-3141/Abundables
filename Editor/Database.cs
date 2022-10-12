747eedebcc5b39c623cf8b5014c1a2f074055d6f
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2022-10-12 19:34:38 -0400

    initial commit

---
+++ Editor/Database.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+
+
+/*  @Todo: rewrite this whole ass thing.
+    Needs to be simplified.
+    
+    The point of this is to have an easy medium between bare AssetBundles
+    and the overcomplicated Addressables. You should have decent control
+    with just a few clicks.
+
+    Users should be able to:
+        - Select Assets to include in a bundle
+        - Create aliases for those Assets that can be used at runtime
+        - Build selected bundles
+   
+
+*/
+
+
+namespace Abundables
+{
+    using Object = UnityEngine.Object;
+
+    public static class Database
+    {
+        public static List<Bundle> bundles = new();
+        public static Bundle currentBundle;
+
+        public static void ChangeCurrentBundle(Bundle bundle)
+        {
+            if(bundle == null)
+            {
+                Debug.LogWarning("Bundle is null!");
+                return;
+            }
+
+            DatabaseWindow.treeDirty = true;
+            currentBundle = bundle;
+        }
+
+
+        public static bool TryCreateBundle(string name, out Bundle bundle, string postfix = "")
+        {
+            bundle = null;
+            bool result = false;
+
+            if(!bundles.Any(b => b.name == name))
+            {
+                bundle = new Bundle(name, postfix);
+                result = true;
+            }
+
+            return result;
+        }
+
+        public static void AddToCurrentBundle(BundleEntry entry)
+        {
+            if(bundles.Count < 1)
+            {
+                bundles.Add(new Bundle("Default Bundle"));
+                currentBundle = bundles[0];
+            }
+
+            currentBundle.TryAdd(entry);
+            DatabaseWindow.treeDirty = true;
+        }
+
+        public static bool Contains(string assetGuid, out Bundle bundle, out BundleEntry entry)
+        {
+            bool result = false;
+
+            bundle = null;
+            entry = null;
+
+            for (int i = 0; i < bundles.Count; i++)
+            {
+                entry = bundles[i].entries.Find(e => e.guid == assetGuid);
+                if (entry != null)
+                {
+                    result = true;
+                    bundle = bundles[i];
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+    public class Bundle
+    {
+        public string name;
+        public string postfix;
+
+        public List<BundleEntry> entries = new List<BundleEntry>();
+
+        public Bundle(string name, string postfix = "")
+        {
+            this.name = name;
+            this.postfix = postfix;
+        }
+
+        public bool TryAdd(BundleEntry entry)
+        {
+            bool result = false;
+
+            if(!entries.Any(e => e.guid == entry.guid))
+            {
+                result = true;
+                entries.Add(entry);
+                DatabaseWindow.treeDirty = true;
+            }
+
+            return result;
+        }
+
+        public bool Remove(BundleEntry entry)
+        {
+            bool result = entries.Remove(entry);
+            
+            if(result)
+                DatabaseWindow.treeDirty = true;
+            
+            return result;
+        }
+    }
+
+    public class BundleEntry
+    {
+        public string assetPath;
+        public string bundleAddress;
+        public string guid;
+
+        public BundleEntry(Object obj, string assetPath, string abundleAddress)
+        {
+            this.assetPath = assetPath;
+            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out guid, out long _);
+            this.bundleAddress = abundleAddress;
+        }
+
+        public BundleEntry(string assetPath, string abundleAddress, string guid)
+        {
+            this.assetPath = assetPath;
+            this.bundleAddress = abundleAddress;
+            this.guid = guid;
+        }
+    }
+}
---
+++ Editor/DatabaseWindow.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace Abundables
+{
+    public class DatabaseWindow : EditorWindow
+    {
+        public static bool treeDirty = false;
+
+        private static DatabaseWindow window;
+        
+        [SerializeField] private TreeViewState treeState;
+        private BundleEntryTree dbTree;
+
+        private const int toolbarHeight = 20;
+
+
+        [MenuItem("Abundables/Database Window")]
+        private static void ShowWindow()
+        {
+            window = GetWindow<DatabaseWindow>();
+            window.titleContent = new GUIContent("Abundables Database");
+            window.Show();
+        }
+
+        public static void ShowAbundable(Bundle bundle, BundleEntry entry)
+        {
+            Database.ChangeCurrentBundle(bundle);
+            ShowWindow();
+        }
+
+        public static void ReloadTree()
+        {
+            if(window != null)
+            {
+                window.dbTree.Reload();
+            }
+        }
+
+        private void OnEnable()
+        {
+            if(treeState == null)
+            {
+                treeState = new TreeViewState();
+            }
+
+            dbTree = new BundleEntryTree(treeState);
+        }
+
+        private void OnDestroy()
+        {
+            window = null;
+        }
+
+        private void OnGUI()
+        {
+            if (treeDirty)
+            {
+                ReloadTree();
+                treeDirty = false;
+            }
+
+            DrawToolbar(new Rect(position.xMin, position.yMin, position.width, toolbarHeight));
+            dbTree.OnGUI(new Rect(position.xMin, position.yMin - toolbarHeight, position.width, position.height /*- toolbarHeight*/));
+        }
+
+        private void DrawToolbar(Rect toolbarPos)
+        {
+            GUILayout.BeginArea(new Rect(0, 0, toolbarPos.width, toolbarHeight));
+
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            {
+
+                var content = new GUIContent("Bundle: " + Database.currentBundle?.name ?? "None", "Change selected bundle");
+                Rect r = GUILayoutUtility.GetRect(content, EditorStyles.toolbarDropDown, GUILayout.ExpandWidth(false));
+                if (EditorGUI.DropdownButton(r, content, FocusType.Passive, EditorStyles.toolbarDropDown))
+                {
+                    var menu = new GenericMenu();
+
+                    foreach(var bundy in Database.bundles)
+                    {
+                        if(bundy != null)
+                        {
+                            menu.AddItem(new GUIContent(bundy.name), false, x => Database.ChangeCurrentBundle(x as Bundle), bundy);
+                        }
+                    }
+
+                    menu.DropDown(r);
+                }
+
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        private class BundleEntryTree : TreeView
+        {
+            public BundleEntryTree(TreeViewState state) : base(state)
+            {
+                Reload();
+            }
+
+            protected override TreeViewItem BuildRoot()
+            {
+                var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
+                var allItems = new List<TreeViewItem>();
+
+                for (int i = 0; i < Database.currentBundle.entries.Count; i++)
+                {
+                    BundleEntry entry = Database.currentBundle.entries[i];
+                    allItems.Add(new TreeViewItem(i, 0, $"Address: {entry.bundleAddress} | Path: {entry.assetPath}"));
+                }
+
+                SetupParentsAndChildrenFromDepths(root, allItems);
+                return root;
+            }
+        }
+    }
+}
