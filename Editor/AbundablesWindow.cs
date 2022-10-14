5662cd757e0c46890083141242743d4cb5cdb3b9
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2022-10-13 21:43:20 -0400

    window half done

--- Editor/AbundablesWindow.cs
+++ Editor/AbundablesWindow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Abundables
@@ -11,6 +13,15 @@ namespace Abundables
         private static AbundableData data;
 
         private const string dataPath = "Assets/Abundables/Editor/Data/AbundableData.asset";
+
+        private float sideBtnWidth = 26f;
+        private float toolbarHeight = 20f;
+        private Color lineColor = new Color(35f / 255f, 35f / 255f, 35f / 255f);
+
+        private List<bool> selectedBundles = new();
+
+
+        #region Window Functions
 
         [InitializeOnLoadMethod]
         private static void OnLoad()
@@ -44,104 +55,183 @@ namespace Abundables
             }
         }
 
-        private void OnGUI()
-        {
-            EditorGUILayout.BeginHorizontal();
-            
-            EditorGUILayout.BeginVertical(EditorStyles.helpBox, GUILayout.MaxWidth(150f), GUILayout.ExpandHeight(true));
-            DrawBundlesPanel();
-            EditorGUILayout.EndVertical();
-            
-            
-            EditorGUILayout.BeginVertical(EditorStyles.helpBox, GUILayout.ExpandHeight(true));
-            DrawAssetsPanel();
-            EditorGUILayout.EndVertical();
-
-            EditorGUILayout.EndHorizontal();
-        }
-
         public void GetSelectedBundles()
         {
             throw new NotImplementedException();
         }
 
+        #endregion
+
+        private void OnGUI()
+        {
+            DrawBundlesPanel();
+            DrawAssetsPanel();
+
+            EditorGUI.DrawRect(new Rect(position.width / 3f, 0f, 1f, position.height), lineColor);
+            EditorGUI.DrawRect(new Rect((position.width / 3f) * 2f - 1f, toolbarHeight, 1f, position.height), lineColor);
+            EditorGUI.DrawRect(new Rect(position.width / 3f, toolbarHeight, (position.width / 3f) * 2f, 1f), lineColor);
+        }
+
         private void DrawBundlesPanel()
         {
-            GUILayout.Label("Bundles");
+            var bpRect = new Rect(0f, 0f, position.width / 3f, position.height);
+            GUILayout.BeginArea(bpRect, FullEditorStyles.Toolbar);
 
-            EditorGUILayout.BeginVertical();
+            #region Toolbar
+            //GUILayout.BeginArea(new Rect(0f, 0f, bpRect.width, toolbarHeight), FullEditorStyles.Toolbar);
 
-            //string[] bundleNames = AssetDatabase.GetAllAssetBundleNames();
-            
-            foreach (var bundle in data.GetBundles())
+            EditorGUI.DropdownButton(new Rect(0f, 0f, sideBtnWidth, toolbarHeight), EditorGUIUtility.TrIconContent("d_Toolbar Plus", "Create new Bundle"), FocusType.Passive, FullEditorStyles.Toolbarbutton);
+            EditorGUI.LabelField(new Rect(sideBtnWidth, 0f, bpRect.width - sideBtnWidth, toolbarHeight), "Bundles", FullEditorStyles.Toolbarbutton.CenterText());
+
+
+            //GUILayout.EndArea();
+            #endregion
+
+            #region List
+            var listRect = new Rect(0, toolbarHeight, bpRect.width, bpRect.height - toolbarHeight * 2);
             {
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.Toggle(false, GUILayout.ExpandWidth(true));
-                
-                if (GUILayout.Button(bundle.name))
+                GUILayout.BeginArea(listRect);
+
+                var toggleWidth = GUI.skin.toggle.CalcSize(new GUIContent("")).x;
+                var togglePosX = (sideBtnWidth / 2f) - (toggleWidth / 2f);
+
+                // Use one of the GUI.Scroll funcs
+                //GUI.BeginScrollView(listRect, default(Vector2), default(Rect), false, true);
+
+
+                var bundles = data.GetBundles();
+                for (int i = 0; i < bundles.Length; i++)
                 {
-                    openedBundle = bundle;
+                    var yPos = toolbarHeight * i;
+
+                    bool toggleVal = false;
+                    try
+                    {
+                        toggleVal = selectedBundles[i];
+                    }
+                    catch
+                    {
+                        selectedBundles.Insert(i, toggleVal);
+                    }
+
+                    selectedBundles[i] = EditorGUI.Toggle(new Rect(togglePosX, yPos, sideBtnWidth, toolbarHeight), toggleVal);
+
+                    var oldCol = GUI.color;
+
+                    if(openedBundle == bundles[i])
+                    {
+                        GUI.color = Color.cyan;
+                    }
+
+                    if(GUI.Button(new Rect(sideBtnWidth, yPos, listRect.width - sideBtnWidth, toolbarHeight), bundles[i].name, FullEditorStyles.MiniButton.CenterText()))
+                    {
+                        openedBundle = bundles[i];
+                    }
+
+                    GUI.color = oldCol;
                 }
 
-                EditorGUILayout.EndHorizontal();
+
+                GUILayout.EndArea();
             }
+            #endregion
 
-            GUILayout.FlexibleSpace();
-
-            EditorGUILayout.BeginHorizontal();
-
-            EditorGUILayout.Toggle(false);
-
-            if(GUILayout.Button("Build Selected"))
+            #region Build buttons
             {
-                AbundablesBuilder.BuildBundle(openedBundle);
+                GUILayout.BeginArea(new Rect(0, listRect.height + toolbarHeight, bpRect.width, toolbarHeight));
+
+                var bStyle = FullEditorStyles.Toolbarbutton.CenterText();
+
+                List<string> selectBtns = new() { "All", "None" };
+                var bRects = EditorGUIUtility.GetFlowLayoutedRects(new Rect(0f, 0f, bpRect.width / 2f, toolbarHeight), bStyle, 0f, 0f, selectBtns);
+
+                if(GUI.Button(bRects[0], new GUIContent(selectBtns[0], "Select all bundles for build"), bStyle))
+                {
+                    for (int i = 0; i < selectedBundles.Count; i++)
+                    {
+                        selectedBundles[i] = true;
+                    }
+                }
+
+                if(GUI.Button(bRects[1], new GUIContent(selectBtns[1], "Deselect all bundles for build"), bStyle))
+                {
+                    for (int i = 0; i < selectedBundles.Count; i++)
+                    {
+                        selectedBundles[i] = false;
+                    }
+                }
+
+                bool anySelected = selectedBundles.Any(sb => sb == true);
+                var buildBtnPosX = bRects[0].width + bRects[1].width;
+
+                EditorGUI.BeginDisabledGroup(!anySelected && openedBundle == null);
+
+                string btnTxt = anySelected ? "Build Selected" : "Build Current";
+                if(GUI.Button(new Rect(buildBtnPosX, 0f, bpRect.width - buildBtnPosX, toolbarHeight), btnTxt, bStyle))
+                {
+                    if (anySelected)
+                    {
+                        var selected = new List<Bundle>();
+                        var bundles = data.GetBundles();
+
+                        for (int i = 0; i < selectedBundles.Count; i++)
+                        {
+                            if (selectedBundles[i])
+                                selected.Add(bundles[i]);
+                        }
+
+                        AbundablesBuilder.BuildBundles(selected.ToArray());
+                    }
+                    else
+                    {
+                        AbundablesBuilder.BuildBundle(openedBundle);
+                    }
+                }
+
+                EditorGUI.EndDisabledGroup();
+
+                GUILayout.EndArea();
             }
+            #endregion
 
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.EndVertical();
+            GUILayout.EndArea();
         }
 
         private void DrawAssetsPanel()
         {
-            GUILayout.Label("Assets");
+            var apRect = new Rect(position.width / 3f, 0f, position.width - (position.width / 3f), position.height);
+            GUILayout.BeginArea(apRect, FullEditorStyles.Toolbar);
+
+            EditorGUI.LabelField(new Rect(0f, 0f, apRect.width, toolbarHeight), "Assets", FullEditorStyles.Toolbarbutton.CenterText());
+
+            //GUILayout.BeginArea(new Rect(0f, toolbarHeight, apRect.width, toolbarHeight));
+
+            EditorGUI.LabelField(new Rect(0f, toolbarHeight, apRect.width / 2f, toolbarHeight), "Address", FullEditorStyles.Toolbarbutton.CenterText());
+            EditorGUI.LabelField(new Rect(apRect.width / 2f, toolbarHeight, apRect.width / 2f, toolbarHeight), "Asset Path", FullEditorStyles.Toolbarbutton.CenterText());
+
+            //GUILayout.EndArea();
 
             if(openedBundle == null)
             {
+                GUILayout.EndArea();
                 return;
             }
 
-            EditorGUILayout.BeginHorizontal();
 
-
-            // Addresses
-            EditorGUILayout.BeginVertical(EditorStyles.helpBox,  GUILayout.ExpandHeight(true));
-
-            GUILayout.Label("Address");
+            var startY = toolbarHeight * 2f;
             for (int i = 0; i < openedBundle.entries.Count; i++)
             {
-                openedBundle.entries[i].address = EditorGUILayout.DelayedTextField(openedBundle.entries[i].address, GUILayout.MinWidth(100f));
+                var entry = openedBundle.entries[i];
+                entry.address = EditorGUI.DelayedTextField(new Rect(0f, startY + (toolbarHeight * i), apRect.width / 2f, toolbarHeight), entry.address);
+
+                EditorGUI.SelectableLabel(new Rect(apRect.width / 2f, startY + (toolbarHeight * i), apRect.width / 2f, toolbarHeight), entry.assetPath);
             }
 
-            EditorGUILayout.EndVertical();
 
-            // Asset Paths
-            EditorGUILayout.BeginVertical(EditorStyles.helpBox,  GUILayout.ExpandHeight(true));
 
-            GUILayout.Label("Asset Path");
-            foreach(var e in openedBundle.entries)
-            {
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Label(e.assetPath);
-                //GUILayout.FlexibleSpace();
-                //EditorGUIUtility.
-                //EditorGUILayout.ObjectField(obj: AssetDatabase.LoadMainAssetAtPath(path), typeof(UnityEngine.Object), false);
-                EditorGUILayout.EndHorizontal();
-            }
-
-            EditorGUILayout.EndVertical();
-            
-            EditorGUILayout.EndHorizontal();
+            GUILayout.EndArea();
         }
+
+        
     }
 }
---
+++ Editor/FullEditorStyles.cs
@@ -0,0 +1,115 @@
+﻿// Written by joshcamas (https://gist.github.com/joshcamas/7443938f14bc7866e40c52e72a71bc47)
+// Adapted by Parzival
+
+using UnityEngine;
+using UnityEditor;
+
+public static class FullEditorStyles
+{
+    public static GUIStyle CenterText(this GUIStyle style)
+    {
+        return new GUIStyle(style) { alignment = TextAnchor.MiddleCenter };
+    }
+
+    public static readonly GUIStyle CNBox = new GUIStyle("CN Box");
+    public static readonly GUIStyle CNEntryInfo = new GUIStyle("CN EntryInfo");
+    public static readonly GUIStyle CNEntryWarn = new GUIStyle("CN EntryWarn");
+    public static readonly GUIStyle CNEntryError = new GUIStyle("CN EntryError");
+    public static readonly GUIStyle CNEntryBackEven = new GUIStyle("CN EntryBackEven");
+    public static readonly GUIStyle CNEntryBackodd = new GUIStyle("CN EntryBackodd");
+    public static readonly GUIStyle CNMessage = new GUIStyle("CN Message");
+    public static readonly GUIStyle CNStatusError = new GUIStyle("CN StatusError");
+    public static readonly GUIStyle CNStatusWarn = new GUIStyle("CN StatusWarn");
+    public static readonly GUIStyle CNStatusInfo = new GUIStyle("CN StatusInfo");
+    public static readonly GUIStyle CNCountBadge = new GUIStyle("CN CountBadge");
+    public static readonly GUIStyle Box = new GUIStyle("Box");
+    //public static readonly GUIStyle LogStyle = new GUIStyle("LogStyle");
+    //public static readonly GUIStyle WarningStyle = new GUIStyle("WarningStyle");
+    //public static readonly GUIStyle ErrorStyle = new GUIStyle("ErrorStyle");
+    //public static readonly GUIStyle EvenBackground = new GUIStyle("EvenBackground");
+    //public static readonly GUIStyle OddBackground = new GUIStyle("OddBackground");
+    //public static readonly GUIStyle MessageStyle = new GUIStyle("MessageStyle");
+    //public static readonly GUIStyle StatusError = new GUIStyle("StatusError");
+    //public static readonly GUIStyle StatusWarn = new GUIStyle("StatusWarn");
+    //public static readonly GUIStyle StatusLog = new GUIStyle("StatusLog");
+    //public static readonly GUIStyle CountBadge = new GUIStyle("CountBadge");
+    public static readonly GUIStyle InBigTitle = new GUIStyle("In BigTitle");
+    public static readonly GUIStyle MiniLabel = new GUIStyle("miniLabel");
+    public static readonly GUIStyle LargeLabel = new GUIStyle("LargeLabel");
+    public static readonly GUIStyle BoldLabel = new GUIStyle("BoldLabel");
+    public static readonly GUIStyle MiniBoldLabel = new GUIStyle("MiniBoldLabel");
+    public static readonly GUIStyle WordWrappedLabel = new GUIStyle("WordWrappedLabel");
+    public static readonly GUIStyle WordWrappedMiniLabel = new GUIStyle("WordWrappedMiniLabel");
+    public static readonly GUIStyle WhiteLabel = new GUIStyle("WhiteLabel");
+    public static readonly GUIStyle WhiteMiniLabel = new GUIStyle("WhiteMiniLabel");
+    public static readonly GUIStyle WhiteLargeLabel = new GUIStyle("WhiteLargeLabel");
+    public static readonly GUIStyle WhiteBoldLabel = new GUIStyle("WhiteBoldLabel");
+    public static readonly GUIStyle MiniTextField = new GUIStyle("MiniTextField");
+    public static readonly GUIStyle Radio = new GUIStyle("Radio");
+    public static readonly GUIStyle MiniButton = new GUIStyle("miniButton");
+    public static readonly GUIStyle MiniButtonLeft = new GUIStyle("miniButtonLeft");
+    public static readonly GUIStyle MiniButtonMid = new GUIStyle("miniButtonMid");
+    public static readonly GUIStyle MiniButtonRight = new GUIStyle("miniButtonRight");
+    public static readonly GUIStyle Toolbar = new GUIStyle("toolbar");
+    public static readonly GUIStyle Toolbarbutton = new GUIStyle("toolbarbutton");
+    public static readonly GUIStyle ToolbarPopup = new GUIStyle("toolbarPopup");
+    public static readonly GUIStyle ToolbarDropDown = new GUIStyle("toolbarDropDown");
+    public static readonly GUIStyle ToolbarTextField = new GUIStyle("toolbarTextField");
+    public static readonly GUIStyle ToolbarSeachTextField = new GUIStyle("ToolbarSeachTextField");
+    public static readonly GUIStyle ToolbarSeachTextFieldPopup = new GUIStyle("ToolbarSeachTextFieldPopup");
+    public static readonly GUIStyle ToolbarSeachCancelButton = new GUIStyle("ToolbarSeachCancelButton");
+    public static readonly GUIStyle ToolbarSeachCancelButtonEmpty = new GUIStyle("ToolbarSeachCancelButtonEmpty");
+    public static readonly GUIStyle SearchTextField = new GUIStyle("SearchTextField");
+    public static readonly GUIStyle SearchCancelButton = new GUIStyle("SearchCancelButton");
+    public static readonly GUIStyle SearchCancelButtonEmpty = new GUIStyle("SearchCancelButtonEmpty");
+    public static readonly GUIStyle HelpBox = new GUIStyle("HelpBox");
+    public static readonly GUIStyle AssetLabel = new GUIStyle("AssetLabel");
+    public static readonly GUIStyle AssetLabelPartial = new GUIStyle("AssetLabel Partial");
+    public static readonly GUIStyle AssetLabelIcon = new GUIStyle("AssetLabel Icon");
+    public static readonly GUIStyle SelectionRect = new GUIStyle("selectionRect");
+    public static readonly GUIStyle MinMaxHorizontalSliderThumb = new GUIStyle("MinMaxHorizontalSliderThumb");
+    public static readonly GUIStyle DropDownButton = new GUIStyle("DropDownButton");
+    public static readonly GUIStyle Label = new GUIStyle("Label");
+    public static readonly GUIStyle ProgressBarBack = new GUIStyle("ProgressBarBack");
+    public static readonly GUIStyle ProgressBarBar = new GUIStyle("ProgressBarBar");
+    public static readonly GUIStyle ProgressBarText = new GUIStyle("ProgressBarText");
+    public static readonly GUIStyle FoldoutPreDrop = new GUIStyle("FoldoutPreDrop");
+    public static readonly GUIStyle INTitle = new GUIStyle("IN Title");
+    public static readonly GUIStyle INTitleText = new GUIStyle("IN TitleText");
+    public static readonly GUIStyle BoldToggle = new GUIStyle("BoldToggle");
+    public static readonly GUIStyle Tooltip = new GUIStyle("Tooltip");
+    public static readonly GUIStyle NotificationText = new GUIStyle("NotificationText");
+    public static readonly GUIStyle NotificationBackground = new GUIStyle("NotificationBackground");
+    public static readonly GUIStyle MiniPopup = new GUIStyle("MiniPopup");
+    public static readonly GUIStyle TextField = new GUIStyle("textField");
+    public static readonly GUIStyle ControlLabel = new GUIStyle("ControlLabel");
+    public static readonly GUIStyle ObjectField = new GUIStyle("ObjectField");
+    public static readonly GUIStyle ObjectFieldThumb = new GUIStyle("ObjectFieldThumb");
+    public static readonly GUIStyle ObjectFieldMiniThumb = new GUIStyle("ObjectFieldMiniThumb");
+    public static readonly GUIStyle Toggle = new GUIStyle("Toggle");
+    public static readonly GUIStyle ToggleMixed = new GUIStyle("ToggleMixed");
+    public static readonly GUIStyle ColorField = new GUIStyle("ColorField");
+    public static readonly GUIStyle Foldout = new GUIStyle("Foldout");
+    public static readonly GUIStyle TextFieldDropDown = new GUIStyle("TextFieldDropDown");
+    public static readonly GUIStyle TextFieldDropDownText = new GUIStyle("TextFieldDropDownText");
+    public static readonly GUIStyle PRLabel = new GUIStyle("PR Label");
+    public static readonly GUIStyle ProjectBrowserGridLabel = new GUIStyle("ProjectBrowserGridLabel");
+    public static readonly GUIStyle ObjectPickerResultsGrid = new GUIStyle("ObjectPickerResultsGrid");
+    public static readonly GUIStyle ObjectPickerBackground = new GUIStyle("ObjectPickerBackground");
+    public static readonly GUIStyle ObjectPickerPreviewBackground = new GUIStyle("ObjectPickerPreviewBackground");
+    public static readonly GUIStyle ProjectBrowserHeaderBgMiddle = new GUIStyle("ProjectBrowserHeaderBgMiddle");
+    public static readonly GUIStyle ProjectBrowserHeaderBgTop = new GUIStyle("ProjectBrowserHeaderBgTop");
+    public static readonly GUIStyle ObjectPickerToolbar = new GUIStyle("ObjectPickerToolbar");
+    public static readonly GUIStyle PRTextField = new GUIStyle("PR TextField");
+    public static readonly GUIStyle PRPing = new GUIStyle("PR Ping");
+    public static readonly GUIStyle ProjectBrowserIconDropShadow = new GUIStyle("ProjectBrowserIconDropShadow");
+    public static readonly GUIStyle ProjectBrowserTextureIconDropShadow = new GUIStyle("ProjectBrowserTextureIconDropShadow");
+    public static readonly GUIStyle ProjectBrowserIconAreaBg = new GUIStyle("ProjectBrowserIconAreaBg");
+    public static readonly GUIStyle ProjectBrowserPreviewBg = new GUIStyle("ProjectBrowserPreviewBg");
+    public static readonly GUIStyle ProjectBrowserSubAssetBg = new GUIStyle("ProjectBrowserSubAssetBg");
+    public static readonly GUIStyle ProjectBrowserSubAssetBgOpenEnded = new GUIStyle("ProjectBrowserSubAssetBgOpenEnded");
+    public static readonly GUIStyle ProjectBrowserSubAssetBgCloseEnded = new GUIStyle("ProjectBrowserSubAssetBgCloseEnded");
+    public static readonly GUIStyle ProjectBrowserSubAssetBgMiddle = new GUIStyle("ProjectBrowserSubAssetBgMiddle");
+    public static readonly GUIStyle ProjectBrowserSubAssetBgDivider = new GUIStyle("ProjectBrowserSubAssetBgDivider");
+    public static readonly GUIStyle ProjectBrowserSubAssetExpandBtn = new GUIStyle("ProjectBrowserSubAssetExpandBtn");
+}
